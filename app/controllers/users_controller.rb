53429995dc6707a8e68e6ecb57720704e6b2ad28
Naozumi Numata <naozumi225@gmail.com>
2020-05-18 00:35:28 +0900

    Add image posting function

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+    
+    def index
+        @users = User.all
+    end
+    def new
+        @user = User.new
+    end
+    def create
+        User.create(user_params)
+        redirect_to :action => :index
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:image)
+    end
+end
